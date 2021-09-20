776cd52e493fedbbecbc6dd540f7437b28f7a1ac
themhz <themhz@gmail.com>
2021-09-20 22:07:16 +0300

    fixing menu remembering state

--- views/layouts/js/menu.js
+++ views/layouts/js/menu.js
@@ -1,8 +1,12 @@
 class Menu{
     //list = '<ul class="navbar-nav">';
     list
+    liid
+    clickedli
     constructor() {
         let self = this;
+        this.liid = 0;
+        this.clickedli = [];
         document.addEventListener('readystatechange', function(evt) {
             if (evt.target.readyState == "complete") {
                 self.list = document.createElement('ul');
@@ -16,6 +20,7 @@ class Menu{
                     
                         self.buildUlLi(tree, self.list);
                         document.getElementById('menu').append(self.list);
+                        self.loadMenuState();
                     }
                 };
                 xhttp.open("GET", "http://localhost:8080/?method=getmenu", true);
@@ -48,13 +53,10 @@ class Menu{
         return root;
     }
 
-
-
-
-
     buildUlLi(tree, ul) {
         let self = this;
         let display = "";
+        this.liid ++;
         tree.children.forEach(function callbackFn(element, index, array) {
             if(parseInt(element.lvl) > 0)
                 display = "none";
@@ -62,6 +64,7 @@ class Menu{
                 display = "";
 
             let li = document.createElement('li');
+            li.id = 'li'+self.liid;
             li.classList.add("nav-item");
             li.classList.add("level"+element.lvl);
             li.style.display = display;
@@ -93,10 +96,10 @@ class Menu{
     }
 
     openMenuItem(obj, lvl){
-      let items = obj.parentElement;
-
+        let items = obj.parentElement;
         let ul = items.querySelector('ul');
         let li = ul.querySelectorAll("li.level"+(parseInt(lvl)+1));
+        this.saveMenuState(items);
 
         for (let item of li) {
             if(item.style.display == ""){
@@ -104,7 +107,32 @@ class Menu{
             }else{
                 item.style.display="";
             }
+        }
 
+    }
+
+    saveMenuState(items){
+        const index = this.clickedli.indexOf(items.id);
+        if (index > -1) {
+            this.clickedli.splice(index, 1);
+        }else{
+            this.clickedli.push(items.id);
+        }
+        localStorage.setItem('menustate', this.clickedli);
+
+        console.log(localStorage.getItem('menustate'));
+    }
+
+    loadMenuState(){
+        let items = localStorage.getItem('menustate').split(",");
+        console.log(items);
+        let obj = "";
+        for(let item of items){
+            obj = document.querySelector("#"+item);
+            let lvl = obj.className.split(" ")[1].replace("level","");
+            this.openMenuItem(obj, lvl);
+            //obj.style.display = "";
+            //console.log();
         }
     }
 }
