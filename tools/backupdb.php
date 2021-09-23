43b6e31d547d8c90350776ed8d3dba2cd40efa99
themhz <themhz@gmail.com>
2021-09-23 21:00:33 +0300

    fixing images and product details on product page

--- tools/backupdb.php
+++ tools/backupdb.php
@@ -8,16 +8,18 @@ require ("../components/handlers/Database.php");
 
 $date = new \DateTime();
 
-$db = Database::getInstance();
-$sql = "SHOW DATABASES";
+//$db = Database::getInstance();
+//$sql = "SHOW DATABASES";
+////
+//$sth = $db->dbh->prepare($sql);
+//$sth->execute();
+//$results = $sth->fetchAll(\PDO::FETCH_OBJ);
 //
-$sth = $db->dbh->prepare($sql);
-$sth->execute();
-$results = $sth->fetchAll(\PDO::FETCH_OBJ);
-
-foreach($results as $schema){
-    echo "backing up ".$schema->Database."\n";
-    $database = $schema->Database;
-    exec("C:\\xampp\\mysql\\bin\\mysqldump --user=root --password= --result-file=\"C:\\xampp\\htdocs\\mywebshop\\db\\".$database."_".$date->format('Ymd_H_i_s').".sql\" $schema->Database");
-}
+//foreach($results as $schema){
+//    echo "backing up ".$schema->Database."\n";
+//    $database = $schema->Database;
+//    exec("C:\\xampp\\mysql\\bin\\mysqldump --user=root --password= --result-file=\"C:\\xampp\\htdocs\\mywebshop\\db\\".$database."_".$date->format('Ymd_H_i_s').".sql\" $schema->Database");
+//}
+$database = "mywebshop";
+exec("C:\\xampp\\mysql\\bin\\mysqldump --user=root --password= --result-file=\"C:\\xampp\\htdocs\\mywebshop\\db\\".$database."_".$date->format('Ymd_H_i_s').".sql\" $database");
 
